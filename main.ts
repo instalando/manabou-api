8e8e22c4fc6cde7d968d91611910b88718cc02c8
instalando <instalando@protonmail.com>
2022-04-20 07:01:55 -0300

    change port

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ server.get('/fetch_word', async (request, reply) => {
   return 'Hello World!'
 })
 
-server.listen(80, (err, address) => {
+server.listen(443, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
