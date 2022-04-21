8fb269d27f3d41391ca648ba3880b3d18509ea08
instalando <instalando@protonmail.com>
2022-04-20 21:07:22 -0300

    set default port

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ server.get('/fetch_word', async (request, reply) => {
   return 'Hello World!'
 })
 
-server.listen(process.env.PORT || 8080, '0.0.0.0', (err, address) => {
+server.listen(process.env.PORT || 3000, '0.0.0.0', (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
