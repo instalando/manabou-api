1509b9e320c9e619b0fd2191504050f0fe337a47
instalando <instalando@protonmail.com>
2022-04-26 21:16:36 -0300

    Melhorado o algoritmo para organizar as posições das palavras

--- src/searchWord.ts
+++ src/searchWord.ts
@@ -37,22 +37,24 @@ const db = knex({
 
 const fetchWordData = async (word: string) => {
     const res = await db('entry')
+        .where('kanji.value', 'LIKE', `${word}%`)
+        .orWhere('kana.value', 'LIKE', `${word}%`)
         .join('kanji', 'entry.id', '=', 'kanji.entry_id')
         .join('kana', 'entry.id', '=', 'kana.entry_id')
         .join('sense', 'entry.id', '=', 'sense.entry_id')
         .join('definition', 'sense.id', '=', 'definition.sense_id')
         .join('part_of_speech', 'sense.id', '=', 'part_of_speech.sense_id')
-        .where('kanji.value', 'LIKE', `${word}%`)
-        .orWhere('kana.value', 'LIKE', `${word}%`)
         .select({
             entry_id: 'entry.id',
             sense_id: 'sense.id',
+            kanji_id: 'kanji.id',
             kanji: 'kanji.value',
+            kana_id: 'kana.id',
             kana: 'kana.value',
             definition: 'definition.value',
             part_of_speech: 'part_of_speech.value'
         })
-        .orderBy('kanji')
+        .orderBy('kanji_id')
 
     return res
 }
